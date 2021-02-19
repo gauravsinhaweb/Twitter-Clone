12db8e042edb672d5a159d04015efebfe8b52846
gauravsinha18 <59147380+gauravsinha18@users.noreply.github.com>
2021-02-19 21:35:30 +0530

    'done'

--- src/Twitter/Feed/TweetBox/TweetBox.js
+++ src/Twitter/Feed/TweetBox/TweetBox.js
@@ -1,6 +1,6 @@
-import { Avatar, Button } from "@material-ui/core";
 import React, { useState } from "react";
 import "./TweetBox.css";
+import { Avatar, Button } from "@material-ui/core";
 import {
   AiOutlinePicture,
   AiOutlineSchedule,
@@ -11,48 +11,57 @@ import { GrEmoji } from "react-icons/gr";
 import db from "../../../firebase";
 
 function TweetBox() {
-  const [tweetmessage, setTweetmessage] = useState("");
+  const [tweetMessage, setTweetMessage] = useState("");
 
-  const postTweet = (e) => {
+  const sendTweet = (e) => {
     e.preventDefault();
+
     db.collection("posts").add({
       displayName: "gaurav sinha",
       username: "gauravsinhaweb",
       verified: true,
-      text: tweetmessage,
+      text: tweetMessage,
       image: "https://media.giphy.com/media/UWnvhKHZNBSgRqVbhB/giphy.gif",
       avatar:
         "https://avatars.githubusercontent.com/u/75125943?s=400&u=211c424499aee235a8fd0532ca232c01a670b6e4&v=4",
     });
-    setTweetmessage("");
+
+    setTweetMessage("");
   };
+
   return (
     <div className="tweetBox">
       <form>
         <div className="tweetBox__input">
           <Avatar src="https://avatars.githubusercontent.com/u/75125943?s=400&u=211c424499aee235a8fd0532ca232c01a670b6e4&v=4" />
           <input
-            onChange={(e) => setTweetmessage(e.target.value)}
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
             placeholder="What's happening?"
             type="text"
           />
         </div>
         <div className="tweetBox__iconBox">
-          {" "}
-          <AiOutlinePicture className="tweetBox__icons" />
+          <span class="image-upload">
+            <label for="file-input">
+              <AiOutlinePicture className="tweetBox__icons" />
+            </label>
+
+            <input id="file-input" type="file" />
+          </span>
           <AiOutlineGif className="tweetBox__icons" />
           <BiPoll className="tweetBox__icons" />
           <GrEmoji className="tweetBox__icons" />
           <AiOutlineSchedule className="tweetBox__icons" />
-          <Button
-            onClick={postTweet}
-            type="submit"
-            className="tweetBox__tweetButton"
-            disabled={!tweetmessage}
-          >
-            Tweet
-          </Button>
         </div>
+        <Button
+          onClick={sendTweet}
+          type="submit"
+          className="tweetBox__tweetButton"
+          disabled={!tweetMessage}
+        >
+          Tweet
+        </Button>
       </form>
     </div>
   );
